e7f7a61a717c5ef2dc5f46c40665d49ea7dbf760
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2021-04-24 17:30:39 -0400

    Step 1 New Functions
    Created showPage, appendElement, and deviseElement functions for step 1 of the project.

--- js/script.js
+++ js/script.js
@@ -18,6 +18,48 @@ Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+// Must use quotation marks when entering arguments into the function. Otherwise it won't work.
+function deviseElement( element, nameofclass ) {
+   const createdElement = document.createElement(element);
+   createdElement.className = nameofclass;
+   return createdElement;
+}
+ 
+function appendElement( parent, child ) {
+   parent.appendChild(child);
+}
+
+function showPage(list, page) {
+   const startIndex = (page * 9) - 9;
+   const endIndex = page * 9;
+   const studentListUL = document.getElementsByClassName('student-list');
+   studentListUL.innerHTML = '';
+   for ( i = 0; i < list.length; i++ ) {
+      if ( i >= startIndex && i <= endIndex ) {
+         const list_student = deviseElement( 'li', 'student-item cf' );
+         const div_student = deviseElement( 'div', 'student-details' );
+         const img_student = deviseElement( 'img', 'avatar' );
+         const heading_three = deviseElement( 'h3', '' );
+         heading_three.textContent = list[i];
+         const span_student = deviseElement( 'span', 'email' );
+         const div_student_two = deviseElement( 'div', 'joined-details' );
+         const span_student_two = document.createElement( 'span', 'date' );
+         appendElement( div_student , img_student );
+         appendElement( div_student , heading_three );
+         appendElement( div_student , span_student );
+         appendElement( div_student_two , span_student_two );
+         appendElement( list_student , div_student );
+         appendElement( list_student , div_student_two );
+
+         studentListUL.insertAdjacentHTML( 'beforeend' , list_student );
+
+      }
+
+
+   }
+
+
+}
 
 
 /*
